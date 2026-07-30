0999e1118a53d9f994b9f4d76faeaa3d276bf6b0
soph4638 <sophieermitnavn@gmail.com>
2026-07-30 22:30:37 +0200

    Create site-config.ts

---
+++ lib/site-config.ts
@@ -0,0 +1,10 @@
+// Kontaktoplysninger vist på bekræftelsessiden efter en booking.
+// Ret trygt tallene/teksten herunder - de bruges automatisk på siden.
+export const CONTACT_INFO = {
+  // Dit Instagram-brugernavn (vises som tekst).
+  instagramHandle: "@zoeganails",
+  // Link til din Instagram-profil.
+  instagramUrl: "https://www.instagram.com/zoeganails",
+  // Dit telefonnummer - ret dette til dit eget nummer.
+  phoneNumber: "12 34 56 78",
+};
