b43313a145eedcfe0d79dd1730cb0ccf98f1cb88
soph4638 <sophieermitnavn@gmail.com>
2026-08-05 23:41:42 +0200

    Gør Z-bogstavet endnu større

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
                         </div>
                 
                         <div className="flex flex-col items-center justify-center text-center">
-                                  <div className="bg-[url('/images/gallery/negle-11.jpg')] bg-cover bg-center bg-clip-text font-archivo text-[380px] font-black leading-[0.72] text-transparent sm:text-[520px]">
+                                  <div className="bg-[url('/images/gallery/negle-11.jpg')] bg-cover bg-center bg-clip-text font-archivo text-[500px] font-black leading-[0.65] text-transparent sm:text-[680px]">
                                               Z
                                   </div>
                                   <a
