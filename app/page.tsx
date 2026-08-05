8bafa809463c3003b0cfaf4621d3078fb0cffb7e
soph4638 <sophieermitnavn@gmail.com>
2026-08-05 23:30:51 +0200

    Gør selve Z-bogstavet større, mindre white space

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
                         </div>
                 
                         <div className="flex flex-col items-center justify-center text-center">
-                                  <div className="bg-[url('/images/gallery/negle-11.jpg')] bg-cover bg-center bg-clip-text font-archivo text-[260px] font-black leading-[0.78] text-transparent sm:text-[340px]">
+                                  <div className="bg-[url('/images/gallery/negle-11.jpg')] bg-cover bg-center bg-clip-text font-archivo text-[380px] font-black leading-[0.72] text-transparent sm:text-[520px]">
                                               Z
                                   </div>
                                   <a
